06e81c82c1018f8d8418e086f657c0ded84dc2d0
Kgiamalis <42589771+kgiamalis@users.noreply.github.com>
2023-09-08 01:12:01 +0300

    Update streamlit_ready_script.py

--- streamlit_ready_script.py
+++ streamlit_ready_script.py
@@ -52,8 +52,12 @@ llm_model = "gpt-3.5-turbo" if current_date > target_date else "gpt-3.5-turbo-03
 file = 'C:\\Users\\30698\\Desktop\\test\\cleaned_updated_manual_sample.csv'
 loader = CSVLoader(file_path=file, encoding='utf-8')
 
-# Convert into vector
-index = VectorstoreIndexCreator(vectorstore_cls=DocArrayInMemorySearch).from_loaders([loader])
+api_key = st.secrets["openai"]["openai_api_key"]
+
+index = VectorstoreIndexCreator(
+    vectorstore_cls=DocArrayInMemorySearch,
+    openai_api_key=api_key  # passing the API key directly
+).from_loaders([loader])
 
 # Generate response
 response = index.query(query)
