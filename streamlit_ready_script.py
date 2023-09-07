df024c9a25b447aca7a525837fa418c33b064e9c
Kgiamalis <42589771+kgiamalis@users.noreply.github.com>
2023-09-08 01:07:40 +0300

    Update streamlit_ready_script.py

--- streamlit_ready_script.py
+++ streamlit_ready_script.py
@@ -1,63 +1,61 @@
 import streamlit as st
+import os
+import openai
+import datetime
+from langchain.chains import RetrievalQA
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import CSVLoader
+from langchain.vectorstores import DocArrayInMemorySearch
+from langchain.indexes import VectorstoreIndexCreator
 
-def load_css(file_name):
-    with open(file_name, 'r') as file:
-        return file.read()
-
-custom_css = load_css("styles.css")
+# Embed the custom CSS directly in the Streamlit app
+custom_css = """
+body {
+    font-family: 'Segoe UI', sans-serif;
+    background-color: #f7f8fa;
+}
+.stMarkdown {
+    background-color: #ffffff;
+    padding: 16px;
+    border-radius: 8px;
+    box-shadow: 0 1px 4px rgba(0, 0, 0, 0.1);
+}
+.stButton > button {
+    background-color: #0079bf;
+    border-radius: 8px;
+    color: white;
+    border: none;
+}
+"""
 st.markdown(f'<style>{custom_css}</style>', unsafe_allow_html=True)
 
 # Streamlit app title and introduction
 st.title("Agile Marketing Q&A")
-st.write("Provide your question below and get answers based on the book:/n Agile Marketing -from waterfall to water flow-.")
+st.write("Provide your question below and get answers based on the book:\n Agile Marketing -from waterfall to water flow-.")
 
 # User Input for the question
 query = st.text_input("Enter your question:", key="unique_query_key")
 
-#Login to OpenAI using a file.
-import os
-import openai
-
-# Fetch the API key from environment variables
-api_key = os.environ.get("OPENAI_API_KEY")
+# Fetch the API key from Streamlit secrets
+api_key = st.secrets["openai"]["openai_api_key"]
 
 # Check if the API key exists
 if not api_key:
     raise ValueError("OpenAI API key not found!")
 
-# account for deprecation of LLM model
-import datetime
-# Get the current date
+# Account for deprecation of LLM model by setting the model based on the current date
 current_date = datetime.datetime.now().date()
-
-# Define the date after which the model should be set to "gpt-3.5-turbo"
 target_date = datetime.date(2024, 6, 12)
+llm_model = "gpt-3.5-turbo" if current_date > target_date else "gpt-3.5-turbo-0301"
 
-# Set the model variable based on the current date
-if current_date > target_date:
-    llm_model = "gpt-3.5-turbo"
-else:
-    llm_model = "gpt-3.5-turbo-0301"
-
-#import langchain libraries
-from langchain.chains import RetrievalQA
-from langchain.chat_models import ChatOpenAI
-from langchain.document_loaders import CSVLoader
-from langchain.vectorstores import DocArrayInMemorySearch
-
-#load file
-file = 'C:\\Users\\30698\Desktop\\test\\cleaned_updated_manual_sample.csv'
+# Load your file (update the path as needed)
+file = 'C:\\Users\\30698\\Desktop\\test\\cleaned_updated_manual_sample.csv'
 loader = CSVLoader(file_path=file, encoding='utf-8')
 
-#Convert into vector
-from langchain.indexes import VectorstoreIndexCreator
+# Convert into vector
+index = VectorstoreIndexCreator(vectorstore_cls=DocArrayInMemorySearch).from_loaders([loader])
 
-index = VectorstoreIndexCreator(
-    vectorstore_cls=DocArrayInMemorySearch
-).from_loaders([loader])
-
-
-#Generate response.
+# Generate response
 response = index.query(query)
 
 st.write(response)
