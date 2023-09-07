87250bc7c614ac42b597522cdfa4393878af5784
Kgiamalis <42589771+kgiamalis@users.noreply.github.com>
2023-09-08 01:19:01 +0300

    Update streamlit_ready_script.py

--- streamlit_ready_script.py
+++ streamlit_ready_script.py
@@ -36,6 +36,30 @@ st.write("Provide your question below and get answers based on the book:\n Agile
 # User Input for the question
 query = st.text_input("Enter your question:", key="unique_query_key")
 
+# Display the hosted image from Cloudinary in the center of the UI
+st.image("https://asset.cloudinary.com/dqp8dgwvy/4a8f29fcc4b30247a72d76248854f243", use_column_width=True)
+
+# Add a clickable link to Amazon with an icon
+st.markdown("[![Buy on Amazon](https://iconarchive.com/download/i82942/google/noto-emoji-animals-nature/22215-cat.ico)](https://www.amazon.de/-/en/Konstantinos-Giamalis/dp/6180023735/?&_encoding=UTF8&tag=kgiamalis-21&linkCode=ur2&linkId=ba4eaff10ab7d658db964e48125abc7d&camp=1638&creative=6742) **Buy on Amazon**", unsafe_allow_html=True)
+
+# Display a header for the questions section
+st.markdown("## Try these questions")
+
+# List of questions
+questions = [
+    "What is Nero?",
+    "Why is it called Nero?",
+    "What are the advantages of Nero?",
+    "Who should read this?",
+    "What are the processes of Nero?",
+    "What is Nero Master?",
+    "Give me a list of reasons why Marketing should become Agile?"
+]
+
+# Loop through the questions and display them
+for q in questions:
+    st.markdown(f"### {q}")  # Display the question as a header
+
 # Fetch the API key from Streamlit secrets
 api_key = st.secrets["openai"]["openai_api_key"]
 
